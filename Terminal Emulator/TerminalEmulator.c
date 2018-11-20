8229656a3cf1eb585daf79a01cc2a9f95bb5d202
Joshua Lu <jolly3092@gmail.com>
2018-11-20 13:32:45 -0600

    Renamed Practice Program to Terminal Emulator

---
+++ Terminal Emulator/TerminalEmulator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    struct Account{ //Creates the account setup and login
+        char username[20];
+        char password[50];
+        char FullName[50];
+    };
+    int loadingtime; //Makes loading seem like ., .., ...
+    while(loadingtime < 5){
+        printf("Loading.")
+    }
+    
+}
